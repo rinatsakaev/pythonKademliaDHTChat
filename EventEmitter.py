88c257e09ffcd1723bc4a17efa8ffe8524d0b71f
rinatsakaev <r.sakaevv@gmail.com>
2018-11-04 20:26:16 +0500

    public chats

--- EventEmitter.py
+++ EventEmitter.py
@@ -14,5 +14,13 @@ class EventEmitter(Thread):
         while True:
             if len(self.msg_queue) is not 0:
                 new_message = self.msg_queue.pop()
-                self.io.emit('message', {'msg': base64.b64decode(new_message.content).decode("utf-8"),
-                                         'sender_id': new_message.sender_node.id})
+                public_mark_index = new_message.content.find('>')
+                if public_mark_index == -1:
+                    self.io.emit('message', {'msg': base64.b64decode(new_message.content).decode("utf-8"),
+                                            'sender_id': new_message.sender_node.id})
+                else:
+                    sender_id = new_message.content[1:public_mark_index]
+                    content = new_message.content[public_mark_index+1:]
+                    self.io.emit('public_message', {'msg': base64.b64decode(content).decode("utf-8"),
+                                             'public_node_id': new_message.sender_node.id,
+                                             'sender_id': sender_id})
